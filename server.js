b37fcf4b3b2cfe27aa7f9b01b6dad01596fd33d4
chinmayee-vaidya <vchinu25@gmail.com>
2015-11-30 15:17:12 -0500

    Making changes in server.js

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ var bodyParser = require('body-parser');
 var app = express();
 
 var mongoose=require('mongoose');
-var connectionString = 'mongodb://127.0.0.1:27017/cs5610';
+var connectionString = 'mongodb://127.0.0.1:27017/test';
 
 if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + "admin" +
-        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "31bGW98LvwM3" +
-        process.env.OPENSHIFT_MONGODB_DB_HOST + '127.0.0.1' +
-        process.env.OPENSHIFT_MONGODB_DB_PORT + '27017' +
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
         process.env.OPENSHIFT_APP_NAME;
 }
  mongoose.connect(connectionString);
