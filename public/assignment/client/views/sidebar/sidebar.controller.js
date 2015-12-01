7fe1b74a2905631155f9edd4b06c735fb3a8968f
chinmayee-vaidya <vchinu25@gmail.com>
2015-11-30 19:49:53 -0500

    data deleted

--- public/assignment/client/views/sidebar/sidebar.controller.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    "use strict";
-    angular.module("FormBuilderApp")
-        .controller("SidebarController", SidebarController);
-
-    function SidebarController($scope, $location) {
-        $scope.$location = $location;
-    }
-})();
